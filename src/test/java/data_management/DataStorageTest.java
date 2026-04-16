145b74165025aad3415e008bbc6cbc077994b78b
Angelos92 <a.sofroniou@student.maastrichtuniversity.nl>
2026-04-16 19:21:26 +0200

    cleaning the code

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -3,12 +3,9 @@ package data_management;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import com.data_management.DataReader;
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 class DataStorageTest {
